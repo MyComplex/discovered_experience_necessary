f08b39b062cb45d1cafd612718288ec4a84b8cfa
Jesus Santos <jo_nez@hotmail.com>
2023-06-12 14:38:05 -0700

    last commit before fixing everything

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,46 +1,87 @@
 /* Global variables */
 var key = '75647ee159a46bf37c329db8acbd31f1';
 var form = document.getElementById('search');
-// var queryLatLong;
-// var weatherNow;
-// var weatherForcast;
+var button = document.getElementById('submit');
+var nowContainer = document.getElementById('current');
+var nowTitle = document.getElementsByClassName('nowTitle');
+var nowTemp = document.getElementsByClassName('nowTemp');
+var nowIcon = document.getElementsByClassName('nowIcon');
+var nowWind = document.getElementsByClassName('nowWind');
+var nowHumidity = document.getElementsByClassName('nowHumidity');
+var nowTitleContent;
+var nowTempContent;
+var nowIconContent;
+var nowWindContent;
+var nowHumidityContent;
+var currentData = [];
 
-/* Form submittal */
-function formSubmit(event) {
-
+/* Submittal */
+button.addEventListener('click', function (event) {
+    event.preventDefault();
     /* Search field input */
     var city = document.getElementById('input').value;
-
     /* Handle if search field is blank */
     if (!city) {
         var errorText = 'Entering a city is required!';
         errorModal(errorText);
         return;
     }
+    if (city) {
+        var current = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${key}&units=imperial`;
+        fetch(current)
+            .then(function (reponse) {
+                return reponse.json();
+            })
+            .then(function (data) {
+                currentData = data;
 
+
+            })
+
+        nowContainer.style.display = 'block';
+        // var forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&appid=${key}&units=imperial`;
+
+        // fetch(latlong)
+        //     .then(function (reponse) {
+        //         return reponse.json();
+        //     })
+        //     .then(function (data) {
+        //         return queryLatLong = data;
+        //     })
+
+        // fetch(forecast)
+        //     .then(function (reponse) {
+        //         return reponse.json();
+        //     })
+        //     .then(function (data) {
+        //         return weatherForcast = data;
+        //     })
+    }
     /* Retrieve history from localStorage if it exists */
     var previousCitiesList = localStorage.getItem('searchList');
     var previousCitiesList = JSON.parse(citiesListString);
-
     if (previousCitiesList === null) {
         previousCitiesList = [];
     }
-
-    /* Trying to check if search already exists in previousCitiesList */
-    // for (let i = 0; i < previousCitiesList.length; i++) {
-    //     var existingCitySearch = previousCitiesList[i];
-
-    // }
-
     /* Push search value to staging array */
     previousCitiesList.push({
         city: city
     });
-
     /* Push staged array to localStorage */
     var previousCitiesListAsString = JSON.stringify(previousCitiesList);
     localStorage.setItem('searchList', previousCitiesListAsString);
-};
+});
+
+nowTitle.textContent = `${currentData.name}`;
+// nowTempContent = `${data.main.temp}°F`;
+// nowIconContent = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+// nowWindContent = `${data.wind.speed} MPH`;
+// nowHumidityContent = `${data.main.humidity}%`;
+console.log(nowTitle.textContent);
+// console.log(nowTempContent);
+// console.log(nowIconContent);
+// console.log(nowWindContent);
+// console.log(nowHumidityContent);
 
 /* Dynamically populate search history */
 var citiesListString = localStorage.getItem('searchList');
@@ -50,7 +91,6 @@ var searchHolder = document.getElementById('histcont');
 if (previousCitiesList) {
     for (var i = 0; i < previousCitiesList.length; i++) {
         var previousCity = previousCitiesList[i];
-// if ()
         var searchListItem = document.createElement('li');
         searchListItem.setAttribute('id', 'sh' + [i]);
 
@@ -62,49 +102,9 @@ if (previousCitiesList) {
     }
 }
 
-/*
-    var latlong = `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&appid=${key}`;
-
-    var current = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${key}&units=imperial`;
-
-    var forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&appid=${key}&units=imperial`;
-
-    fetch(latlong)
-        .then(function (reponse) {
-            return reponse.json();
-        })
-        .then(function (data) {
-            return queryLatLong = data;
-        })
-
-    fetch(current)
-        .then(function (reponse) {
-            return reponse.json();
-        })
-        .then(function (data) {
-            return weatherNow = data;
-        })
-
-    fetch(forecast)
-        .then(function (reponse) {
-            return reponse.json();
-        })
-        .then(function (data) {
-            return weatherForcast = data;
-        })
-
-    // localStorage.setItem()
-
-}
-
-console.log(queryCount);
-console.log(queryLatLong);
-console.log(weatherNow);
-console.log(weatherForcast);
-*/
 
 /* Handle search submission */
-form.addEventListener('submit', formSubmit);
+// form.addEventListener('submit', formSubmit);
 
 /* Handle submission from history */
 
@@ -119,12 +119,6 @@ function refreshTime() {
     timeDisplay.textContent = dateString;
 }
 setInterval(refreshTime, 1000);
-
-const d1 = dayjs().add(1, 'day').format('ddd, MMM D');
-const d2 = dayjs().add(2, 'day').format('ddd, MMM D');
-const d3 = dayjs().add(3, 'day').format('ddd, MMM D');
-const d4 = dayjs().add(4, 'day').format('ddd, MMM D');
-const d5 = dayjs().add(5, 'day').format('ddd, MMM D');
 
 /* Modal for error handling */
 // Get the modal
